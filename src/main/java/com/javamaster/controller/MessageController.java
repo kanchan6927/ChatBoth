e7e38e97918abec62f05c1aba575d58026a84ba8
kanchanchavan2511@gmail.com <kanchanchavan2511@gmail.com>
2023-02-21 11:59:54 +0530

    Initial commit

---
+++ src/main/java/com/javamaster/controller/MessageController.java
@@ -0,0 +1,47 @@
+package com.javamaster.controller;
+
+import com.javamaster.model.MessageModel;
+import com.javamaster.storage.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")              // for private
+    public void sendMessage(@DestinationVariable String to, MessageModel message) {
+        System.out.println("handling send message: " + message + " to: " + to);
+        boolean isExists = UserStorage.getInstance().getUsers().contains(to);
+        if (isExists) {
+            simpMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
+        }
+    }
+
+    @MessageMapping("/message")
+    @SendTo("/groupChat/public")
+
+    public Message receivePublicMessage(@Payload Message message){
+        return message;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
